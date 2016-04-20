4fe1cc0b4de6063162936e1af32c1d287a62e4b5
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-04-20 09:49:06 +0100

    set up environments

--- nightwatch/nightwatch.conf.js
+++ nightwatch/nightwatch.conf.js
@@ -1,6 +1,8 @@
-// var seleniumServer = require('selenium-server')
-// var phantomjs = require('phantomjs-prebuilt')
-// var chromedriver = require('chromedriver')
+const seleniumServer = require('selenium-server')
+const chromedriver = require('chromedriver')
+
+const in_docker = process.env.SELENIUM_HOST !== undefined;
+const selenium_host = process.env.SELENIUM_HOST || "localhost"
 
 module.exports = {
   src_folders: [require('nightwatch-cucumber')()],
@@ -10,27 +12,45 @@ module.exports = {
   page_objects_path: '',
   live_output: false,
   disable_colors: false,
-  // test_workers: {
-  //  enabled: true,
-  //  workers: 'auto'
-  // },
 
   selenium: {
-    start_process: false,
-    // server_path: seleniumServer.path,
+    start_process: !in_docker,
+    server_path: seleniumServer.path,
     log_path: 'reports',
     host: 'selenium',
     port: 4444,
-    // cli_args: {
-    //   'webdriver.chrome.driver': chromedriver.path
-    // }
+    cli_args: {
+      'webdriver.chrome.driver': chromedriver.path
+    }
   },
 
   test_settings: {
+    dev: {
+      launch_url: 'https://wallboard-ircbd-dev.notprod.homeoffice.gov.uk',
+      globals: {
+        backend_url: 'https://api-ircbd-dev.notprod.homeoffice.gov.uk',
+      }
+    },
+    int: {
+      launch_url: 'https://wallboard-ircbd-int.notprod.homeoffice.gov.uk',
+      globals: {
+        backend_url: 'https://api-ircbd-int.notprod.homeoffice.gov.uk',
+      }
+    },
+    uat: {
+      launch_url: 'https://wallboard-ircbd-uat.notprod.homeoffice.gov.uk',
+      globals: {
+        backend_url: 'https://api-ircbd-uat.notprod.homeoffice.gov.uk',
+      }
+    },
+
     default: {
-      launch_url: 'http://localhost',
+      launch_url: 'http://localhost:8080',
+      globals: {
+        backend_url: 'http://localhost:8000',
+      },
       selenium_port: 4444,
-      selenium_host: 'selenium',
+      selenium_host: selenium_host,
       silent: true,
       screenshots: {
         enabled: true,
@@ -42,8 +62,8 @@ module.exports = {
         browserName: 'chrome',
         javascriptEnabled: true,
         acceptSslCerts: true,
-        chromeOptions : {
-          "args" : ["--no-sandbox"]
+        chromeOptions: {
+          "args": ["--no-sandbox"]
         }
       },
     },
