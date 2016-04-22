f68549f1e48513468c29093014ae2f51968f0409
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-04-22 16:08:22 +0100

    move some of the messy xpath into the page objects

--- nightwatch/features/step_definitions/steps.js
+++ nightwatch/features/step_definitions/steps.js
@@ -9,20 +9,16 @@ module.exports = function () {
   })
 
   this.Then(/^I should be connected$/, function () {
-    this.expect.element(".disconnected").to.not.present.after(2000);
+    this.page.wallboard().expect.element("@disconnected_message").to.not.present.after(2000);
 
   });
 
   this.Then(/^The Centre "([^"]*)" should show the following under "([^"]*)":$/, function (centre_name, gender, table) {
-    let centreToggle = `//h3/text()[contains(., "${centre_name}")]/ancestor::div[contains(@class, "centre")]/centre-gender-directive[@gender="'${gender}'"]//a[contains(@class, "detail-toggle")]`;
-    this.useXpath();
-    this.expect.element(centreToggle).to.be.present.after(1000);
-    this.click(centreToggle);
+    this.page.wallboard().toggleCentreDetails(centre_name, gender);
     _.map(table.rowsHash(), (v, k) =>
-      this.expect.element(`//h3/text()[contains(., "${centre_name}")]/ancestor::div[contains(@class, "centre")]/centre-gender-directive[@gender="'${gender}'"]//td/text()[contains(., "${k}")]/ancestor::tr/td[last()]`).text.to.equal(v).after(1000)
+      this.page.wallboard().expectCentreDetail(centre_name, gender, k, v)
     );
-    this.click(centreToggle);
-    this.useCss();
+    this.page.wallboard().toggleCentreDetails(centre_name, gender);
   });
 
 }
--- nightwatch/page_objects/wallboard.js
+++ nightwatch/page_objects/wallboard.js
@@ -1,7 +1,26 @@
+"use strict";
+
 module.exports = {
+  commands: [{
+    toggleCentreDetails: function (centre_name, gender) {
+      let centreToggle = `//h3/text()[contains(., "${centre_name}")]/ancestor::div[contains(@class, "centre")]/centre-gender-directive[@gender="'${gender}'"]//a[contains(@class, "detail-toggle")]`;
+      this.api.useXpath();
+      this.expect.element(centreToggle).to.be.present.after(1000);
+      this.click(centreToggle);
+      this.api.useCss();
+    },
+    expectCentreDetail: function (centre_name, gender, k, v) {
+      this.api.useXpath();
+      this.expect.element(`//h3/text()[contains(., "${centre_name}")]/ancestor::div[contains(@class, "centre")]/centre-gender-directive[@gender="'${gender}'"]//td/text()[contains(., "${k}")]/ancestor::tr/td[last()]`).text.to.equal(v).before(1000);
+      this.api.useCss();
+    }
+  }],
   elements: {
     title: {
       selector: "h1"
     },
+    disconnected_message: {
+      selector: ".disconnected"
+    }
   }
 };
