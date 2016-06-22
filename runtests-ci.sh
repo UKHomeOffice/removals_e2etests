d5785d51b4c16fcd57876d00dc470c040acbed4c
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-06-22 11:45:50 +0100

    refactor a bit to tolerate unmatched branch

--- runtests-ci.sh
+++ runtests-ci.sh
@@ -8,11 +8,11 @@ rm -rf be fe
 git clone https://github.com/UKHomeOffice/removals_integration.git be
 git clone https://github.com/UKHomeOffice/removals_wallboard.git fe
 cd be
-git checkout $BRANCH
-docker build -t ibm-backend .
+git checkout $BRANCH || true
 cd ../fe
-git checkout $BRANCH
-docker build -t ibm-frontend .
+git checkout $BRANCH || true
 cd ..
+docker build -t ibm-frontend be
+docker build -t ibm-backend fe
 
 ./runtests.sh --env docker
