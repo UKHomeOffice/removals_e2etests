485415667b5248e72b4906efac9d9374ef75e4b8
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-08-10 14:40:06 +0100

    put some travis folding in

--- runtests.sh
+++ runtests.sh
@@ -22,12 +22,15 @@ function tidyup {
 rm -fr nightwatch/reports/* nightwatch/screenshots/default
 mkdir -p nightwatch/reports nightwatch/screenshots/default
 
+echo travis_fold:start:DOCKER_COMPOSE_UP
 docker-compose up -d --build --force-recreate
 
 echo "waiting for everything to be up"
 
 docker wait removalse2etests_waiter_1
 docker-compose logs waiter
+
+echo travis_fold:end:DOCKER_COMPOSE_UP
 
 docker-compose run --rm -T test nightwatch $@
 exitcode=$?
