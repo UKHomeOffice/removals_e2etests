8605fe45498426391b83cc6ab4189c931ddbdf5b
Gabriel Ng <gabrielc@acuma.co.uk>
2018-03-08 21:53:10 +0000

    Ammend cred

--- nightwatch/features/step_definitions/login.js
+++ nightwatch/features/step_definitions/login.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = function () {
-    console.log("username" + process.env.KEYCLOAK_USER + "password" + process.env.KEYCLOAK_PASS)
+    console.log("username: " + process.env.KEYCLOAK_USERA + "\n password:" + process.env.KEYCLOAK_PASSA)
     this.Given(/^I am a logged in user$/, function () {
         if (this.globals.auth_required === false) {
             return true
