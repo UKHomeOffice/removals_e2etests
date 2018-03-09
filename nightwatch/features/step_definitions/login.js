5a0dbf232298ee35768de53a079af3ab2b94fd95
Gabriel Ng <gabrielc@acuma.co.uk>
2018-03-09 11:34:37 +0000

    Login test

--- nightwatch/features/step_definitions/login.js
+++ nightwatch/features/step_definitions/login.js
@@ -2,7 +2,6 @@
 
 module.exports = function () {
   this.Given(/^I am a logged in user$/, function () {
-    console.log('USR:' + process.env.KEYCLOAK_USER + 'PASS: ' + process.env.KEYCLOAK_PASS)
     if (this.globals.auth_required === false) {
       return true
     }
