fb5eb0d1d05434361629c46d609612341ad0622f
Gabriel Ng <gabrielc@acuma.co.uk>
2018-03-09 10:28:44 +0000

    Ammended login ES lint  errors

--- nightwatch/features/step_definitions/login.js
+++ nightwatch/features/step_definitions/login.js
@@ -1,35 +1,35 @@
 'use strict'
 
 module.exports = function () {
-    console.log("Retrieved username: " + process.env.KEYCLOAK_USER + "\n Retrieved password: " + process.env.KEYCLOAK_PASS)
-    this.Given(/^I am a logged in user$/, function () {
-        if (this.globals.auth_required === false) {
-            return true
-        }
-        this
-            .url(this.globals.backend_url)
-        this.page.login().dologin(process.env.KEYCLOAK_USER, process.env.KEYCLOAK_PASS)
-    })
+  this.Given(/^I am a logged in user$/, function () {
+    if (this.globals.auth_required === false) {
+      return true
+    }
+    this
+      .url(this.globals.backend_url)
 
-    this.Given(/^I am an unauthenticated user$/, function () {
-        this
-            .url(this.globals.backend_url)
-            .deleteCookies()
-    })
+    this.page.login().dologin(process.env.KEYCLOAK_USER, process.env.KEYCLOAK_PASS)
+  })
 
-    this.Then(/^I should be redirected to login via keycloak$/, function () {
-        if (this.globals.auth_required === false) {
-            return true
-        }
-        this.page.login().expect.element('@username').to.be.visible.before(5000)
-    })
+  this.Given(/^I am an unauthenticated user$/, function () {
+    this
+      .url(this.globals.backend_url)
+      .deleteCookies()
+  })
 
-    this.Then(/^I login$/, function () {
-        if (this.globals.auth_required === false) {
-            return true
-        }
-        this.page.login().dologin(process.env.KEYCLOAK_USER, process.env.KEYCLOAK_PASS)
+  this.Then(/^I should be redirected to login via keycloak$/, function () {
+    if (this.globals.auth_required === false) {
+      return true
+    }
+    this.page.login().expect.element('@username').to.be.visible.before(5000)
+  })
 
-        this.page.wallboard().expect.element('@title').text.to.equal('IRC Capacity Management').before(1000)
-    })
+  this.Then(/^I login$/, function () {
+    if (this.globals.auth_required === false) {
+      return true
+    }
+    this.page.login().dologin(process.env.KEYCLOAK_USER, process.env.KEYCLOAK_PASS)
+
+    this.page.wallboard().expect.element('@title').text.to.equal('IRC Capacity Management').before(1000)
+  })
 }
